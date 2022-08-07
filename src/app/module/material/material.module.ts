525ad3de45a2aba87d571daa40cfd8ec9ff88b85
krisna <ejikrisnayana@gmail.com>
2022-08-07 14:02:16 +0700

    change theme to dark

--- src/app/module/material/material.module.ts
+++ src/app/module/material/material.module.ts
@@ -7,9 +7,10 @@ import { MatChipsModule } from '@angular/material/chips';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list'; 
+import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
@@ -27,6 +28,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatListModule,
     MatMenuModule,
     MatProgressBarModule,
+    MatSidenavModule,
     MatToolbarModule
   ]
 })
